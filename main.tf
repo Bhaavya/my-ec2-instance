a80888f52fe2ab5500c461d595ac091413668856
Bhaavya <bhavya@ibm.com>
2026-06-02 19:04:37 +0000

    Use aws_ami data source instead of SSM for AMI lookup

--- main.tf
+++ main.tf
@@ -2,12 +2,23 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_ssm_parameter" "al2023_ami" {
-  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+data "aws_ami" "latest_amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64", "amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
 }
 
 locals {
-  resolved_ami_id = var.ami_id != "" ? var.ami_id : data.aws_ssm_parameter.al2023_ami.value
+  resolved_ami_id = var.ami_id != "" ? var.ami_id : data.aws_ami.latest_amazon_linux.id
 }
 
 resource "aws_instance" "this" {
--- outputs.tf
+++ outputs.tf
@@ -17,3 +17,8 @@ output "region" {
   description = "AWS region the EC2 instance is deployed in"
   value       = var.region
 }
+
+output "resolved_ami_id" {
+  description = "AMI ID actually used to launch the instance"
+  value       = local.resolved_ami_id
+}
